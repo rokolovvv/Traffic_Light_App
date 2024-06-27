9b9a4a6e32da18607ce18ec0468fb498c58d03ef
rokolovvv <149207388+rokolovvv@users.noreply.github.com>
2024-06-28 01:53:10 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import { useRef } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 
-function App() {
+function TrafficLight() {
+
+  let currentColor = 'red'
+  const red = useRef(null)
+  const orange = useRef(null)
+  const green = useRef(null)
+
+  function setColor (el, active) {
+    el.current.style.opacity = active ? 1 : 0.4;
+  }
+
+useEffect(() => {
+  setColor(red, true)
+}, [])
+function changecolor() {
+  if(currentColor === 'red' ) {
+    currentColor = 'green'
+    setColor(green, true)
+    setColor(red)
+  } else if (currentColor === 'green') {
+    currentColor = 'orange'
+    setColor(orange, true)
+    setColor(green)
+  } else {
+    currentColor = 'red'
+    setColor(red, true)
+    setColor(orange)
+  }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="TLwrapper">
+      <div className="TL">
+        <div ref={red} id='red' state={true}></div>
+        <div ref={orange} id='orange' state={false}></div>
+        <div ref={green} id='green' state={false}></div>
+      </div>
+      <button onClick={changecolor}>Переключити</button>
     </div>
   );
 }
 
-export default App;
+export default TrafficLight;
